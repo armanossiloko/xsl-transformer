ec9dba5650730a57a3fe8248e9e77e2af5f6414b
Arman Ossi Loko <armanossiloko@gmail.com>
2022-04-10 12:41:06 +0200

    Generalized the use of the tool - Removed IIS freb.xsl - Added an input file flag - Reworked the Transform method to allow cancelling the transformation if a destination file already exists

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # XSL transformer
 
-Since IE is being retired, it is getting a bit harder and more annoying to view IIS' failed request logs (which I have the deep pleasure of having to view very often), I decided to help myself by creating a simple tool that will transform my failed request logs into proper HTML so I can easily view them in another browser.
+Since IE is being retired, it is getting a bit harder and more annoying to view IIS' failed request logs (which I have the deep pleasure of having to view very often), I decided to help myself by creating a simple tool that will transform my failed request logs (or any other transformation data, usually in XML format) into a proper readable file format via an XSL template (in my case, IIS' failed request logs in XML to browser readable HTML so I can easily view them without Internet Explorer).
 
 # Setup:
+
+If you're on Linux or MacOS, see the `Usage` section below. Windows users, aside from that, can additionally define a template path (I'll use IIS' template for transforming failed request logs into HTML as an example):
 
 - Save the freb.xsl (rename it to what you want if needed) somewhere on your PC
 - Preferably set up an environment variable (`XSLT_TEMPLATE_PATH`) to the value of that location (e.g `C:\Windows\System32\freb.xsl`).
@@ -12,17 +14,23 @@ Since IE is being retired, it is getting a bit harder and more annoying to view 
 2. Run `dotnet pack`
 3. Install the tool using `dotnet tool install --global --add-source ./nupkg Xsl.Transformer`
 
-When wanting to remove, use `dotnet tool uninstall --global Xsl.Transformer` to uninstall the tool from your system.
+When willing to remove, use `dotnet tool uninstall --global Xsl.Transformer` to uninstall the tool from your system.
 
 ## Usage:
 
 ```
-Transforms a given IIS failed request log file (usually in .XML format) into a browser readable HTML.
+Transforms a given data file (usually in .XML format, e.g IIS failed request log file) into a human or browser readable file (e.g HTML, XLS, etc.).
 
-Usage: xslt <input-file> [OPTION]
-  -o, --output                  full direct path to a file (preferably that doesn't exist; else it will be overwritten)
+Usage: xslt <input-file> [OPTIONS]
+  -i, --input
   -t, --template                full direct path to an XSL template file
+  -o, --output                  full direct path to a file (preferably that doesn't exist; else it will be overwritten)
+
+You can control the template by either using one of the available flags or by setting an environment variable in your system.
+Just set the environment variable XSLT_TEMPLATE_PATH on machine level to be the full path to the template file.
 
 Example case:
-    xslt freb.xml --output "C:\parsed.html"
+    xslt data.xml --output "C:\transformed.html"
+    xslt data.xml --input "C:\template.xslt" --output "C:\transformed.html"
+    xslt data.xml -i template.xsl -o transformed.xls
 ```
--- Xsl.Transformer/Program.cs
+++ Xsl.Transformer/Program.cs
@@ -1,8 +1,27 @@
 ﻿using System.Xml.XPath;
 using System.Xml.Xsl;
 
-void Transform(string xml, string templatePath, string outputPath = @"freb.html")
+void Transform(string xml, string templatePath, string outputPath = "transformed.html")
 {
+    var cancellationRequested = false;
+    if (File.Exists(outputPath))
+    {
+        Console.Write($"File {outputPath} already exists. Overwrite? (Y/n): ");
+        var command = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(command))
+        {
+            command = string.Empty;
+        }
+
+        cancellationRequested = !command.Equals("Y", StringComparison.OrdinalIgnoreCase);
+    }
+
+    if (cancellationRequested)
+    {
+        Console.WriteLine("Exiting the transformation...");
+        return;
+    }
+
     var xsltArgs = new XsltArgumentList();
 
     using var ms = new FileStream(outputPath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
@@ -22,18 +41,26 @@ void Transform(string xml, string templatePath, string outputPath = @"freb.html"
 
     xslt.Transform(xpd, xsltArgs, ms);
     ms.Seek(0, SeekOrigin.Begin);
+    Console.WriteLine($"File {outputPath} successfully generated.");
 }
 
 void PrintTutorial()
 {
-    Console.WriteLine(@"Transforms a given IIS failed request log file (usually in .XML format) into a browser readable HTML.
+    Console.WriteLine(@"Transforms a given data file (usually in .XML format, e.g IIS failed request log file) into a human or browser readable file (e.g HTML, XLS, etc.).
 
-Usage: xslt <input-file> [OPTION]
-  -o, --output                  full direct path to a file (preferably that doesn't exist; else it will be overwritten)
+Usage: xslt <input-file> [OPTIONS]
+  -i, --input
   -t, --template                full direct path to an XSL template file
+  -o, --output                  full direct path to a file (preferably that doesn't exist; else it will be overwritten)
+
+You can control the template by either using one of the available flags or by setting an environment variable in your system.
+Just set the environment variable XSLT_TEMPLATE_PATH on machine level to be the full path to the template file.
 
 Example case:
-    xslt freb.xml --output ""C:\parsed.html""");
+    xslt data.xml --output ""C:\transformed.html""
+    xslt data.xml --input ""C:\template.xslt"" --output ""C:\transformed.html""
+    xslt data.xml -i template.xsl -o transformed.xls
+");
 }
 
 void PrintError(string error)
@@ -56,9 +83,10 @@ try
 
     // First item should always be the file or help command
     var inputXml = arguments.FirstOrDefault();
-    if (string.IsNullOrWhiteSpace(inputXml))
+    if (string.IsNullOrWhiteSpace(inputXml) || inputXml.StartsWith('-'))
     {
-        PrintError("Provide an input file");
+        PrintError($"No transformation data provided.");
+        PrintTutorial();
         return;
     }
 
@@ -73,41 +101,45 @@ try
 
     if (!File.Exists(inputXml))
     {
-        PrintError($"File {inputXml} doesn't exist");
+        PrintError($"File {inputXml} doesn't exist.");
+        PrintTutorial();
         return;
     }
 
     // The rest are flags with their values
     var flags = new Dictionary<string, string>();
-    if (arguments.Any(arg => arg.StartsWith("-")))
+    if (!arguments.Any(arg => arg.StartsWith("-")))
     {
-        // Retrieve flags with their values, store in a dictionary
-        var flagsWithValues = arguments.Skip(1).ToList();
-        for (int i = 0; i < flagsWithValues.Count - 1; i += 2)
-        {
-            try
-            {
-                flags[flagsWithValues[i]] = flagsWithValues[i + 1];
-            }
-            catch
-            {
-                PrintError("Invalid call, see usage below");
-                PrintTutorial();
-                return;
-            }
-        }
-    }
-    else
-    {
-        PrintError("Invalid call, see usage below");
+        PrintError("Invalid call, see usage below.");
         PrintTutorial();
         return;
     }
 
-    var path = Environment.GetEnvironmentVariable("XSLT_TEMPLATE_PATH", EnvironmentVariableTarget.Machine);
-    if (string.IsNullOrWhiteSpace(path) && !flags.ContainsKey("-t") && !flags.ContainsKey("--template"))
+    // Retrieve flags with their values, store in a dictionary
+    var flagsWithValues = arguments.Skip(1).ToList();
+    for (int i = 0; i < flagsWithValues.Count - 1; i += 2)
     {
-        PrintError("Please set up an XSLT_TEMPLATE_PATH environment variable");
+        try
+        {
+            flags[flagsWithValues[i]] = flagsWithValues[i + 1];
+        }
+        catch
+        {
+            PrintError("Invalid call, see usage below.");
+            PrintTutorial();
+            return;
+        }
+    }
+
+    var path = Environment.GetEnvironmentVariable("XSLT_TEMPLATE_PATH", EnvironmentVariableTarget.Machine);
+    if (string.IsNullOrWhiteSpace(path)
+        && !flags.ContainsKey("-t")
+        && !flags.ContainsKey("--template")
+        && !flags.ContainsKey("-i")
+        && !flags.ContainsKey("--input")
+        )
+    {
+        PrintError("Please set up an XSLT_TEMPLATE_PATH environment variable or use one of the flags for defining a template path.");
         return;
     }
 
@@ -125,6 +157,8 @@ try
                 break;
             case "-t":
             case "--template":
+            case "-i":
+            case "--input":
                 {
                     pathInfo = new FileInfo(item.Value);
                 }
